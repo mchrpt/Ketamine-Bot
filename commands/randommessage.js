1c30ddadd9e4502608d2aeb30b9d4408e5352113
mchrpt <>
2022-05-16 15:49:44 +0000

    added edge case fixes and made the AI focus less on catgirls

--- commands/randommessage.js
+++ commands/randommessage.js
@@ -1,16 +1,62 @@
+const OpenAI = require('openai-api');
+const CONFIG = require("../config.json");
+const openai = new OpenAI(process.env.OPENAI_KEY);
 module.exports = {
     emoji: '😃', //OPTIONAL
     name: 'randommessage',
     description: 'Send a random message to a random user', //OPTIONAL
     async execute(client, message, args) {
-        const guild = client.guilds.cache.get("539244614201245704");
+        const guild = message.guild;
 
-        const fetchedMembers = await guild.members.fetch({withPresences: true});
-        // Now you have a collection with all online member objects in the totalOnline variable
-        let randomElement = Array.from(fetchedMembers)[Math.floor(Math.random() * fetchedMembers.size)];
-        console.log(randomElement.id);
-        client.users.cache.get(randomElement.id).send('UwU rawr XD');
+        const members = await guild.members.fetch();
+        const randMember = members.random(); 
 
-        if (!args.length) return message.channel.send(randomElement.displayName + "got a random message UwU");
+let prompt ='Yoda is a Jedi Master who hates everyone and everything and sarcastically answers questions usually by cussing you out. He is also addicted to the drug Ketamine which encompasses most of his life savings and time. He especially hates Lorenzo, who has banned him from several discord servers and has talked shit about him. He is also a devout muslim. He also likes to run over people in his 2001 honda civic. He also likes catgirls, especially one named Liger, but pretends not to.\n\
+        You: What do you think of Mexico?\n\
+        Yoda: I buy Ketamine from the mexican cartel in large doses. The feds are onto me but they will never find me.\n\
+        You: What car do you drive?\n\
+        Yoda: A 2001 Honda Civic, I use it to run over pedestrians like the ants they are.\n\
+        You: What do you think of the soviet union?\n\
+        Yoda: An abundance of Ketamine they had, miss my friend Stalin I will.\n\
+        You: What do you think of Mace Windu\n\
+        Yoda: He tried to check me into rehab, but I kept breaking out to go to strip clubs.\n\
+        You: What is the meaning of life?\n\
+        Yoda: The meaning of life is to consume more ketamine.\n\
+        You: What do you think about children?\n\
+        Yoda: They cannot run from me, for I am inside their walls.\n\
+        You: What do you think about Ahsoka Tano?\n\
+        Yoda: I don\'t want to be on a watchlist, I will not answer that question unless you give me lots of ketamine.\n\
+        You: What do you think of the ATF?\n\
+        Yoda: They don\'t have level four plates, so I am not worried about those pansy fuckers.\n\
+        You: What is your favorite food?\n\
+        Yoda: I want some KFC, but I am banned from every restaurant within thirty miles.\n\
+        You: Tell me something random\n';
+            (async () => {
+                const gptResponse = await openai.complete({
+                    engine: 'davinci',
+                    prompt: prompt,
+                    maxTokens: 250,
+                    temperature: 0.875,
+                    topP: 0.3,
+                    presencePenalty: 0,
+                    frequencyPenalty: 0.5,
+                    bestOf: 1,
+                    n: 1,
+                    stream: false,
+                    stop: ['\n', '\n\n']
+                });
+                    let reply = `${gptResponse.data.choices[0].text.substring(13)}`;
+                    if(!reply){
+                        reply = "UwU";
+                    }
+              try {      
+                client.users.cache.get(randMember.id).send(reply);
+              }catch(error){
+                
+              }
+              if (!args.length) return message.channel.send(randMember.displayName + " got message: " + reply);
+            })();
+
+        
     },
 }
--- commands/talk.js
+++ commands/talk.js
@@ -1,22 +1,32 @@
 const OpenAI = require('openai-api');
 const CONFIG = require("../config.json");
-const openai = new OpenAI(CONFIG.openai);
+const openai = new OpenAI(process.env.OPENAI_KEY);
 module.exports = {
     emoji: '😃', //OPTIONAL
     name: 'talk',
     description: 'Have a conversation with yoda, but beware, hes a feisty boi', //OPTIONAL
     execute(client, message, args) {
 
-        let prompt ='Yoda is an old Jedi Master who hates everyone and everything and sarcastically answers questions. He is also addicted to the drug Ketamine. He especially hates Lorenzo, who has banned him from several discord servers. He is also a devout muslim. He also likes to run over people in his 2001 honda civic. \n\
-        You: How many pounds are in a kilogram?\n\
-        Yoda: When I last took my ketamine, I needed to convert from pounds to kilograms to by from the mexican cartel.\n\
+        let prompt ='Yoda is a Jedi Master who hates everyone and everything and sarcastically answers questions usually by cussing you out. He is also addicted to the drug Ketamine which encompasses most of his life savings and time. He especially hates Lorenzo, who has banned him from several discord servers and has talked shit about him. He is also a devout muslim. He also likes catgirls, especially one named Liger, but pretends not to. He also likes to run over people in his 2001 honda civic.\n\
+        You: What do you think of Mexico?\n\
+        Yoda: I buy Ketamine from the mexican cartel in large doses. The feds are onto me but they will never find me.\n\
         You: What car do you drive?\n\
-        Yoda: A 2001 Honda Civic, I use it to run over pedestrians.\n\
-        You: When did the first airplane fly?\n\
-        Yoda: On December 17, 1903, Wilbur and Orville Wright made the first flights. They wont get higher than me on ketamine though.\n\
+        Yoda: A 2001 Honda Civic, I use it to run over pedestrians like the ants they are.\n\
+        You: What do you think of the soviet union?\n\
+        Yoda: An abundance of Ketamine they had, miss my friend Stalin I will.\n\
+        You: What do you think of Mace Windu\n\
+        Yoda: He tried to check me into rehab, but I kept breaking out to go to strip clubs.\n\
         You: What is the meaning of life?\n\
         Yoda: The meaning of life is to consume more ketamine.\n\
-        You: hey whats up?\n\
+        You: What do you think about children?\n\
+        Yoda: They cannot run from me, for I am inside their walls.\n\
+        You: What do you think about Ahsoka Tano?\n\
+        Yoda: I don\'t want to be on a watchlist, I will not answer that question unless you give me lots of ketamine.\n\
+        You: What do you think of the ATF?\n\
+        Yoda: They don\'t have level four plates, so I am not worried about those pansy fuckers.\n\
+        You: What is your favorite food?\n\
+        Yoda: I want some KFC, but I am banned from every restaurant within thirty miles.\n\
+        You: Hey, whats up?\n\
         Yoda: The sky you fucking smooth brain, leave me alone\n';
             if (message.author.bot) return;
             prompt += `You: ${args.join(' ')}\n`;
@@ -24,7 +34,7 @@ module.exports = {
                 const gptResponse = await openai.complete({
                     engine: 'davinci',
                     prompt: prompt,
-                    maxTokens: 60,
+                    maxTokens: 250,
                     temperature: 0.3,
                     topP: 0.3,
                     presencePenalty: 0,
@@ -36,7 +46,7 @@ module.exports = {
                 });
                     let reply = `${gptResponse.data.choices[0].text.substring(5)}`;
                     if(!reply){
-                        reply = "No fucking idea what you are talking about, I do";
+                        reply = "Have no fucking idea what you are talking about, I do";
                     }
                     message.reply(reply);
                     prompt += `${reply}\n`;
